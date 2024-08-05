80f133e695284659370dec4bc7c50e1e3d9d1dfe
raheel4033 <raheelh717@hotmail.com>
2024-08-06 01:46:49 +0500

    TickerNo4 Controllers Added

---
+++ CodeBase/Controllers/PinCodeController.cs
@@ -0,0 +1,75 @@
+﻿using CodeBase.IServices;
+using CodeBase.Models;
+using CodeBase.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeBase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PinCodeController : ControllerBase
+    {
+        private readonly IPinCodeService _pinCodeService;
+
+        public PinCodeController(IPinCodeService pinCodeService)
+        {
+            _pinCodeService = pinCodeService;
+        }
+        [HttpPost]
+        public IActionResult AddEmailPin(PinCode pinCode)
+        {
+            //WE SHOULD USE CUSTOM EXCEPTIONS INSTEAD OF TRY CATCH
+            try
+            {
+                _pinCodeService.AddEmailCodeService(pinCode);
+                return CreatedAtAction("EmailCodeAdded", pinCode);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpPut]
+        public IActionResult UpdateEmailAccount(PinCode pinCode)
+        {
+            //WE SHOULD USE CUSTOM EXCEPTIONS INSTEAD OF TRY CATCH
+            try
+            {
+                _pinCodeService.UpdateEmailCodeService(pinCode);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAccount(long id)
+        {
+            //WE SHOULD USE CUSTOM EXCEPTIONS INSTEAD OF TRY CATCH
+            try
+            {
+                _pinCodeService.DeleteCodeService(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpPost]
+        [Route("GenerateCode")]
+        public IActionResult GenerateCode(PinCode pinCode)
+        {
+            _pinCodeService.GeneratePhoneCode(pinCode);
+            _pinCodeService.AddEmailCodeService(pinCode);
+            return Ok("Generatred Code");
+        }
+    }
+}
---
+++ CodeBase/Controllers/PrivacyPolicyController.cs
@@ -0,0 +1,33 @@
+﻿using CodeBase.IServices;
+using CodeBase.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeBase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrivacyPolicyController : ControllerBase
+    {
+        private readonly IPrivacyPolicyService _privacyPolicyService;
+        public PrivacyPolicyController(IPrivacyPolicyService privacyPolicyService)
+        {
+                _privacyPolicyService = privacyPolicyService;
+        }
+        [HttpPost]
+        [Route("postbool")]
+        public IActionResult AcceptAggrement(PrivacyPolicy privacyPolicy)
+        {
+            if (privacyPolicy.AgreeToTermsAndConditions)
+            {
+                _privacyPolicyService.AcceptAggrementService(privacyPolicy);
+                return Ok("Return Accepted");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
